c36174b9fedfef6551f0feb9906269888a6beb5d
Jiang <zhedajiangyong@126.com>
2020-02-22 11:09:02 +0800

    add admin

---
+++ Admin.java
@@ -0,0 +1,14 @@
+﻿public class Hello{
+	public static String name = "sss";
+	piblic static void main(Strings[] args){
+		System.out.println("我好 你好");
+		System.out.println("你好， 你好");
+		System.out.println("hello， world");
+		System.out.println("happy new year");
+		System.out.println("新年， kuaile");
+	}
+	
+	public static void print(){
+		System.out.println("dajiahao");
+	}
+}
